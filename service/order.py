a726bd6228df5b0221ba05f860a5c2f335f6e938
frangoxd <89959407+frangoxd@users.noreply.github.com>
2021-09-04 12:13:11 -0300

    Update order.py

--- service/order.py
+++ service/order.py
@@ -6,11 +6,11 @@ def buy_market_order(timestamp, api_key, secret, quantity, price):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=BTTBUSD&side=BUY&quantity={quantity}&price={price}&type=LIMIT&timestamp={timestamp}'
+    query_string = f'symbol=BTTBUSD&side=BUY&quantity={quantity}&type=MARKET&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
-        '/api/v3/order?' + query_string + f'&signature={signature}',
+        '/api/v3/order/test?' + query_string + f'&signature={signature}',
         payload,
         headers,
     )
@@ -22,17 +22,48 @@ def sell_market_order(timestamp, api_key, secret, quantity, price):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=BTTBUSD&side=SELL&quantity={quantity}&price={price}&type=LIMIT&timestamp={timestamp}'
+    query_string = f'symbol=BTTBUSD&side=SELL&quantity={quantity}&type=MARKET&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
-        '/api/v3/order?' + query_string + f'&signature={signature}',
+        '/api/v3/order/test?' + query_string + f'&signature={signature}',
         payload,
         headers,
     )
     res = conn.getresponse()
     return res
 
+
+def buy_limit_order(timestamp, api_key, secret, quantity, price):
+    conn = http.client.HTTPSConnection("api.binance.com")
+    payload = ""
+    headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
+    query_string = f'symbol=BTTBUSD&side=BUY&quantity={quantity}&timeInForce=IOC&price={price}&type=LIMIT&timestamp={timestamp}'
+    signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+    conn.request(
+        "POST",
+        '/api/v3/order/test?' + query_string + f'&signature={signature}',
+        payload,
+        headers,
+    )
+    res = conn.getresponse()
+    return res
+
+
+def sell_limit_order(timestamp, api_key, secret, quantity, price):
+    conn = http.client.HTTPSConnection("api.binance.com")
+    payload = ""
+    headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
+    query_string = f'symbol=BTTBUSD&side=SELL&quantity={quantity}&timeInForce=IOC&price={price}&type=LIMIT&timestamp={timestamp}'
+    signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+    conn.request(
+        "POST",
+        '/api/v3/order/test?' + query_string + f'&signature={signature}',
+        payload,
+        headers,
+    )
+    res = conn.getresponse()
+    return res
 
 
 def send_signed_request(http_method, url_path, payload={}):
