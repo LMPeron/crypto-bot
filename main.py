7aab6d1b7e265492fc08edf75b9474e424ede85b
LMPeron <52711736+LMPeron@users.noreply.github.com>
2021-09-04 11:18:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,75 @@
-import numpy
-from service.utils import get_ping, get_server_time
-from service.statistics import get_candle_stick
+from service.utils import get_server_time
 import time
 from utils.market.klines import get_stat_by_minute, format_stats
 from service.order import buy_market_order, sell_market_order
 import json
-
+from datetime import date, datetime
 
 def main():
     bought = False
     bought_price = 0
     quantity_bought = 0
+    start(bought=bought, bought_price=bought_price, quantity_bought=quantity_bought)
 
+
+def start(bought, bought_price, quantity_bought):
     while True:
-        close, b_val = get_stat_by_minute()
-        quantity = int(round(20 / float(b_val), 3))
-        stats = format_stats(close)
-        f = open("log.txt", "a")
+        try:
+            close, b_val = get_stat_by_minute()
+            quantity = int(round(14 / float(b_val), 3))
+            stats = format_stats(close)
+            f = open("log.txt", "a")
 
-        if stats["rsi"] < 30 and (stats["diff"] > stats["macd"]) and bought == False:
-            print("Compra -->" + b_val)
-            bought = True
-            f.writelines([f"Comprado em --> {b_val}\n"])
-            quantity_bought = quantity
-            bought_price = b_val
-            print(quantity)
-            t = get_server_time()
-            tm = json.load(t)["serverTime"]
-            print(
-                json.load(
-                    buy_market_order(
-                        timestamp=tm,
-                        api_key="",
-                        secret="",
-                        quantity=quantity,
+            # and (stats["diff"] > stats["macd"])
+            if stats["rsi"] < 30 and bought == False:
+                print("Compra -->" + b_val)
+                bought = True
+                f.writelines([f"Comprado em --> {b_val}"])
+                f.writelines([f" || as --> {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}\n"])
+                quantity_bought = quantity
+                bought_price = b_val
+                print(quantity)
+                t = get_server_time()
+                tm = json.load(t)["serverTime"]
+                print(
+                    json.load(
+                        buy_market_order(
+                            timestamp=tm,
+                            api_key="5oGo1UeJhwe2wuZqhrsNPF75RtE0mvfhnC8nT9qzcmJiyq7OsBYXmeRqbeg3aa79",
+                            secret="9eGhTaIMRE70jLnaBb118j1N1WNCfafmV3V5OV8BeqEM0XZ9KDTBaTh9YelFB5oB",
+                            quantity=quantity,
+                            price=b_val
+                        )
                     )
                 )
-            )
 
-        if stats["rsi"] > 70 and bought == True and b_val > bought_price:
-            print("Vende -->" + b_val)
-            bought = False
-            f.writelines([f"Vendido em --> {b_val}\n"])
-            f.writelines(["---------------------\n"])
-            print(quantity_bought)
-            t = get_server_time()
-            tm = json.load(t)["serverTime"]
-            print(
-                json.load(
-                    sell_market_order(
-                        timestamp=tm,
-                        api_key="",
-                        secret="",
-                        quantity=quantity_bought,
+            if stats["rsi"] > 70 and bought == True and b_val > bought_price:
+                print("Vende -->" + b_val)
+                bought = False
+                f.writelines([f"Vendido  em --> {b_val}"])
+                f.writelines([f" || as --> {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}\n"])
+                f.writelines(["---------------------\n\n"])
+                print(quantity_bought)
+                t = get_server_time()
+                tm = json.load(t)["serverTime"]
+                print(
+                    json.load(
+                        sell_market_order(
+                            timestamp=tm,
+                            api_key="5oGo1UeJhwe2wuZqhrsNPF75RtE0mvfhnC8nT9qzcmJiyq7OsBYXmeRqbeg3aa79",
+                            secret="9eGhTaIMRE70jLnaBb118j1N1WNCfafmV3V5OV8BeqEM0XZ9KDTBaTh9YelFB5oB",
+                            quantity=quantity_bought,
+                            price=b_val
+                        )
                     )
                 )
-            )
 
-        time.sleep(1)
 
+            print("Conectado.")
+            time.sleep(2)
+        except:
+            print("Desconectado.")
+            time.sleep(2)
+            start(bought, bought_price, quantity_bought)
 
 main()
