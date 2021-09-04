ff5f3d695abaafb1c678145a6e362d9b0afa32f0
frangoxd <89959407+frangoxd@users.noreply.github.com>
2021-09-04 15:49:10 -0300

    Update klines.py

--- utils/market/klines.py
+++ utils/market/klines.py
@@ -7,19 +7,19 @@ import talib
 
 def get_stat_by_minute():
     try:
-        #   date = datetime.now() + timedelta(minutes=-10)
-        #   date_format = f"{datetime.timestamp(date)}".split(".")
-        #   date_str = (
-        #       date_format[0]
-        #       + f"{date_format[1][0]}{date_format[1][1]}{date_format[1][2]}"
-        #   )
+      #   date = datetime.now() + timedelta(minutes=-10)
+      #   date_format = f"{datetime.timestamp(date)}".split(".")
+      #   date_str = (
+      #       date_format[0]
+      #       + f"{date_format[1][0]}{date_format[1][1]}{date_format[1][2]}"
+      #   )
         res = get_candle_stick("XECBUSD", "1m", "1628202599999")
         close_list = []
-        if res:
+        if (res):
             l = json.load(res)
             l_value = l[len(l) - 1][4]
             for i in l:
-                close_list.append(float(i[4]))
+                  close_list.append(float(i[4]))
 
             num_arr = numpy.array(close_list)
 
@@ -31,15 +31,20 @@ def get_stat_by_minute():
 
 
 def format_stats(close):
-    rsi = talib.RSI(close, timeperiod=6)
-    macd, macdsignal, macdhist = talib.MACD(
-        close, fastperiod=12, slowperiod=26, signalperiod=9
-    )
+      rsi = talib.RSI(close, timeperiod=6)
+      macd, macdsignal, macdhist = talib.MACD(
+            close, fastperiod=12, slowperiod=26, signalperiod=9
+      )
 
-    r_rsi = rsi[len(rsi) - 1]
-    r_dea = "%.8f" % (macd[len(macd) - 1])
-    r_diff = "%.08f" % (macdsignal[len(macdsignal) - 1])
-    r_macd = "%.08f" % (macdhist[len(macdhist) - 1])
+      r_rsi = rsi[len(rsi) - 1]
+      r_dea = "%.8f" % (macd[len(macd) - 1])
+      r_diff = "%.08f" % (macdsignal[len(macdsignal) - 1])
+      r_macd = "%.08f" % (macdhist[len(macdhist) - 1])
 
-    res = {"rsi": r_rsi, "dea": r_dea, "diff": r_diff, "macd": r_macd}
-    return res
+      res = {
+            "rsi": r_rsi,
+            "dea": r_dea,
+            "diff": r_diff,
+            "macd": r_macd
+      }
+      return res
