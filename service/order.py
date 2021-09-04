0190064e8c42f51759d40ccc574a553d951b2a6a
LMPeron <52711736+LMPeron@users.noreply.github.com>
2021-09-04 11:19:23 -0300

    Update order.py

--- service/order.py
+++ service/order.py
@@ -2,15 +2,15 @@ import http.client
 import hmac
 import hashlib
 
-def buy_market_order(timestamp, api_key, secret, quantity):
+def buy_market_order(timestamp, api_key, secret, quantity, price):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=XECBUSD&side=BUY&quantity={quantity}&type=MARKET&timestamp={timestamp}'
+    query_string = f'symbol=BTTBUSD&side=BUY&quantity={quantity}&price={price}&type=LIMIT&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
-        '/api/v3/order/test?' + query_string + f'&signature={signature}',
+        '/api/v3/order?' + query_string + f'&signature={signature}',
         payload,
         headers,
     )
@@ -18,15 +18,15 @@ def buy_market_order(timestamp, api_key, secret, quantity):
     return res
 
 
-def sell_market_order(timestamp, api_key, secret, quantity):
+def sell_market_order(timestamp, api_key, secret, quantity, price):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=XECBUSD&side=SELL&quantity={quantity}&type=MARKET&timestamp={timestamp}'
+    query_string = f'symbol=BTTBUSD&side=SELL&quantity={quantity}&price={price}&type=LIMIT&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
-        '/api/v3/order/test?' + query_string + f'&signature={signature}',
+        '/api/v3/order?' + query_string + f'&signature={signature}',
         payload,
         headers,
     )
