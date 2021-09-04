fdab481d6795e5ebbc2135b33c43b8748fb387d6
frangoxd <89959407+frangoxd@users.noreply.github.com>
2021-09-04 15:49:40 -0300

    Update order.py

--- service/order.py
+++ service/order.py
@@ -2,11 +2,11 @@ import http.client
 import hmac
 import hashlib
 
-def buy_market_order(timestamp, api_key, secret, quantity, price):
+def buy_market_order(timestamp, api_key, secret, quantity):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=BTTBUSD&side=BUY&quantity={quantity}&type=MARKET&timestamp={timestamp}'
+    query_string = f'symbol=XECBUSD&side=BUY&quantity={quantity}&type=MARKET&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
@@ -18,11 +18,11 @@ def buy_market_order(timestamp, api_key, secret, quantity, price):
     return res
 
 
-def sell_market_order(timestamp, api_key, secret, quantity, price):
+def sell_market_order(timestamp, api_key, secret, quantity):
     conn = http.client.HTTPSConnection("api.binance.com")
     payload = ""
     headers = {"Content-Type": "application/json", "X-MBX-APIKEY": api_key}
-    query_string = f'symbol=BTTBUSD&side=SELL&quantity={quantity}&type=MARKET&timestamp={timestamp}'
+    query_string = f'symbol=XECBUSD&side=SELL&quantity={quantity}&type=MARKET&timestamp={timestamp}'
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
@@ -42,7 +42,7 @@ def buy_limit_order(timestamp, api_key, secret, quantity, price):
     signature = hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
     conn.request(
         "POST",
-        '/api/v3/order/test?' + query_string + f'&signature={signature}',
+        '/api/v3/order?' + query_string + f'&signature={signature}',
         payload,
         headers,
     )
@@ -66,15 +66,3 @@ def sell_limit_order(timestamp, api_key, secret, quantity, price):
     return res
 
 
-def send_signed_request(http_method, url_path, payload={}):
-    query_string = urlencode(payload, True)
-    if query_string:
-        query_string = "{}&timestamp={}".format(query_string, get_timestamp())
-    else:
-        query_string = 'timestamp={}'.format(get_timestamp())
-
-    url = BASE_URL + url_path + '?' + query_string + '&signature=' + hashing(query_string)
-    print("{} {}".format(http_method, url))
-    params = {'url': url, 'params': {}}
-    response = dispatch_request(http_method)(**params)
-    return response.json()
